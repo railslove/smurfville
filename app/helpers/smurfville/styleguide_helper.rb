55aa59bd5ee3b70e83a765d73f1dbba7b4ecd301
Jakob Hilden <jakobhilden@gmail.com>
2012-09-23 20:04:47 +0200

    changed default for foreground_color() helper and added tests for it

--- app/helpers/smurfville/styleguide_helper.rb
+++ app/helpers/smurfville/styleguide_helper.rb
@@ -6,9 +6,10 @@ module Smurfville
     # returns well readable, high-contrast foreground color ("black" or "white") based on passed background_color
     def foreground_color(background_color)
       if color = Smurfville::ColorVariableParser.parse_color(background_color)
-        return "black"  if color.brightness > 0.5
+        color.brightness > 0.5 ? "black" : "white"
+      else
+        "black"
       end
-      return "white"
     end
   end
 end
--- test/unit/helpers/smurf/styleguide_helper_test.rb
+++ test/unit/helpers/smurf/styleguide_helper_test.rb
@@ -1,6 +1,23 @@
+# encoding: UTF-8
+
 require 'test_helper'
 
 module Smurfville
   class StyleguideHelperTest < ActionView::TestCase
+    test 'foreground_color' do
+      # dark colors
+      assert_equal "white", foreground_color("red")
+      assert_equal "white", foreground_color("#012123")
+      assert_equal "white", foreground_color("#000")
+
+      # bright colors
+      assert_equal "black", foreground_color("#efefef")
+      assert_equal "black", foreground_color("white")
+
+      # non-colors
+      # => black = default
+      assert_equal "black", foreground_color("türkis")
+      assert_equal "black", foreground_color(nil)
+    end
   end
 end
