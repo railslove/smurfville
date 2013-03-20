67cafee3a25e8634ceec8b5d7a3697f747ff891d
Timo Schilling <timo@schilling.io>
2013-03-20 04:00:23 +0100

    initialize sass_directory per Engine initializer

--- lib/smurfville/engine.rb
+++ lib/smurfville/engine.rb
@@ -1,5 +1,9 @@
 module Smurfville
   class Engine < ::Rails::Engine
     isolate_namespace Smurfville
+
+    initializer "smurfville.sass_directory" do |app|
+      Smurfville.sass_directory = Rails.root.join('app', 'assets', 'stylesheets').to_s
+    end
   end
 end
