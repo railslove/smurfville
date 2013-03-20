3b2b22323ce7fb9f5deff4c6fc0569cdb51a6892
Timo Schilling <timo@schilling.io>
2013-03-20 04:26:38 +0100

    remove smurfville initializer from dummy app

--- test/dummy/config/initializers/smurfville.rb
+++
@@ -1,2 +0,0 @@
-Smurfville.sass_directory = Rails.root.join('app', 'assets', 'stylesheets').to_s
-Smurfville.typography_sass_file = Rails.root.join('app', 'assets', 'stylesheets', '_typography.sass').to_s
