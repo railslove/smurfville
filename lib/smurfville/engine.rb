a665b5c9cfb079d81e4290edbd0b764aeec24ebb
Timo Schilling <timo@schilling.io>
2013-03-20 04:25:37 +0100

    set typography_sass_file in smurfville.sass_directory initializer

--- lib/smurfville/engine.rb
+++ lib/smurfville/engine.rb
@@ -4,6 +4,7 @@ module Smurfville
 
     initializer "smurfville.sass_directory" do |app|
       Smurfville.sass_directory = Rails.root.join('app', 'assets', 'stylesheets').to_s
+      Smurfville.typography_sass_file = Rails.root.join('app', 'assets', 'stylesheets', '_typography.sass').to_s
     end
   end
 end
